6c421acb79aeaa383eeb371fad55b20d169fa781
Manish Sahu <101319723+Manishsahu116@users.noreply.github.com>
2025-01-21 17:08:44 +0530

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -49,7 +49,7 @@ const Hero = () => (
                 <a href="mailto:sahumanish2969@gmail.com">
                     <FaEnvelope className="text-white w-8 h-8 hover:text-blue-500" />
                 </a>
-                <a href="https://drive.google.com/file/d/1Ua1g8WsiF3mBpal75fvGKK47GlsOdgth/view?usp=sharing" download>
+                <a href="https://drive.google.com/file/d/1iqu3tzfug05LbQxjkW8SQzpA69fVUSb-/view?usp=sharing" download>
                     <FaFileDownload className="text-white w-8 h-8 hover:text-blue-500" />
                 </a>
             </div>      
